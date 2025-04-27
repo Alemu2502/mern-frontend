264c90c49fc8188df21c8c609f6899a55c8b58f8
Alemu2502 <fre6819@gnail.com>
2025-04-27 08:29:47 -0600

    update

---
+++ frontend/build/precache-manifest.3ee91aa7ec7e706c61c8924b2712a69d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6c710556e39da5d70cce91a706cc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0524f45d1dfb33e9d8a4",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "38c858811fe8aea78845",
+    "url": "/static/css/main.2b56716f.chunk.css"
+  },
+  {
+    "revision": "0524f45d1dfb33e9d8a4",
+    "url": "/static/js/2.5f2eb04c.chunk.js"
+  },
+  {
+    "revision": "38c858811fe8aea78845",
+    "url": "/static/js/main.9dde9344.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
